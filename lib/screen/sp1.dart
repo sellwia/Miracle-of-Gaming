b9fea1240b3a20e561d646e2aeeb0a71a0debc2a
sellwia <Sellydwikurnia@gmail.com>
2026-01-04 16:01:57 +0700

    Menambahkan Foto

--- lib/screen/sp1.dart
+++ lib/screen/sp1.dart
@@ -8,16 +8,38 @@ class SpScreen1 extends StatelessWidget {
     return Scaffold(
       body: Container(
         // Kasih Warna Background
+        width: double.infinity,
+        height: double.infinity,
         decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                Color(0xFF0F0F0F),
-                Color(0xFF1C1F26),
-                Color(0xFF2A2D34),
-            ],
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF0F0F0F), Color(0xFF1C1F26), Color(0xFF2A2D34)],
           ),
+        ),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 70),
+
+            Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.amber,
+                image: DecorationImage(
+                  image: AssetImage("assets/images/stickps.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 20),
+            
+          ],
         ),
       ),
     );
